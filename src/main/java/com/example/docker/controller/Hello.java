19888d66d5c88a5684966b6c07d640bb8bc536b0
kathirms <kathirvel.m@batonsystems>
2020-11-19 09:10:18 +0530

    hia this

--- src/main/java/com/example/docker/controller/Hello.java
+++ src/main/java/com/example/docker/controller/Hello.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String  getString() {
-      return "hai i from jenkins when push operation final push";
+      return "hai i from jenkins when push operation this my EC2 machine";
   }
 }
