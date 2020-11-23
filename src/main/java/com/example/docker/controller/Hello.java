35b8e8f744681ec6517ef8721e2039bb7cbb95d9
kathirms <kathirvel.m@batonsystems>
2020-11-23 12:55:28 +0530

    hai i am kathir again

--- src/main/java/com/example/docker/controller/Hello.java
+++ src/main/java/com/example/docker/controller/Hello.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String  getString() {
-      return " hai siva hai ";
+      return " hai iam kathir again";
   }
 }
